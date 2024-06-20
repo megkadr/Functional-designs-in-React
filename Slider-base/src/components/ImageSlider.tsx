0d9cca973c5be8163cb9de7dbeea30e6ff5ad1d9
Artur <a.rezmer@sellpander.pl>
2024-06-20 19:12:20 +0200

    #Created basic slider version

--- Slider-base/src/components/ImageSlider.tsx
+++ Slider-base/src/components/ImageSlider.tsx
@@ -1,17 +1,30 @@
 import { useState } from "react"
 import {ArrowBigLeft, ArrowBigRight} from "lucide-react"
 import ImageSliderProps from "../types/ImageSliderProps"
-import Holidays from 'date-holidays';
+import Style from "./ImageSlider.module.css"
+
 export default function ImageSlider({imageUrls}: ImageSliderProps){
     const [imageIndex, setImageIndex] = useState(0)
-    const hd = new Holidays('PL');
-    const holidays = hd.getHolidays(2024);
-    console.log(holidays);
+
+    function showNextImage(){
+        setImageIndex(index =>{
+            if(index === imageUrls.length - 1) return 0
+            return index + 1
+        })
+    }
+
+    function showPrevImage(){
+        setImageIndex(index =>{
+            if(index === 0) return imageUrls.length - 1
+            return index - 1
+        })
+    }
+
     return(
-        <div style={{ maxWidth: "1200px", width: "100%", height: "500px", margin: "0 auto"}}>
-            <img src={imageUrls[imageIndex]} className="img-slider-img" />
-            <button><ArrowBigLeft /></button>
-            <button><ArrowBigRight /></button>
+        <div style={{ minWidth: "1200px", width: "100%", margin: "0 auto", aspectRatio: "12 / 6",position: "relative" }}>
+            <img src={imageUrls[imageIndex]} className={Style.imgSliderImg} />
+            <button onClick={showPrevImage} className={Style.imgSliderBtn} style={{left: 0}}><ArrowBigLeft /></button>
+            <button onClick={showNextImage} className={Style.imgSliderBtn} style={{right: 0}}><ArrowBigRight /></button>
         </div>
     )
 }
