5a3120b8462a5d9eda9ecf2adf2cf2885b7a957b
Artur <a.rezmer@sellpander.pl>
2024-06-20 19:41:31 +0200

    #Adjusted better version of slider with ease in adn out animations

--- Slider-base/src/components/ImageSlider.tsx
+++ Slider-base/src/components/ImageSlider.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react"
-import {ArrowBigLeft, ArrowBigRight} from "lucide-react"
+import {ArrowBigLeft, ArrowBigRight, Circle, CircleDot} from "lucide-react"
 import ImageSliderProps from "../types/ImageSliderProps"
 import Style from "./ImageSlider.module.css"
 
@@ -22,9 +22,18 @@ export default function ImageSlider({imageUrls}: ImageSliderProps){
 
     return(
         <div style={{ minWidth: "1200px", width: "100%", margin: "0 auto", aspectRatio: "12 / 6",position: "relative" }}>
-            <img src={imageUrls[imageIndex]} className={Style.imgSliderImg} />
+            <div style={{width: "100%", height: "100%", display: "flex", overflow: "hidden"}}>
+                {imageUrls.map(url =>(
+                    <img key={url} src={url} className={Style.imgSliderImg} style={{translate: `${-100 * imageIndex}%` }} />
+                ))}
+            </div>
             <button onClick={showPrevImage} className={Style.imgSliderBtn} style={{left: 0}}><ArrowBigLeft /></button>
             <button onClick={showNextImage} className={Style.imgSliderBtn} style={{right: 0}}><ArrowBigRight /></button>
+            <div style={{position: "absolute", bottom: ".5rem", left: "50%", translate: "-50%", display: "flex", gap: ".25rem"}}>
+                {imageUrls.map((_, index)=>(
+                    <button onClick={() => setImageIndex(index)} className={Style.imgSliderBtnDot}>{index === imageIndex ? <CircleDot /> : <Circle />}</button>
+                ))}
+            </div>
         </div>
     )
 }
